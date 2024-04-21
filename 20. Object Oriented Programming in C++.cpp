8f909eea96b42bb7df06085092024ed58e01649b
dan <121939052+spncdan@users.noreply.github.com>
2024-04-21 18:15:02 -0400

    Update 20. Object Oriented Programming in C++.cpp

--- 20. Object Oriented Programming in C++.cpp
+++ 20. Object Oriented Programming in C++.cpp
@@ -26,6 +26,7 @@ class Person {
     public:
         string name;
         int age;
+    
     /* Protected is a third access modifer where it is only accessed within class and derived classes (inherited)
      protected:
         int x;
@@ -36,7 +37,6 @@ class Person {
     Person(string name, int age, string address, int zipcode, string occupation):
         name(name), age(age), address(address), zipcode(zipcode), occupation(occupation){
         //insert code to run when the constructor is called
-        
     }
     
     // Setters allows to set and change existing object's properties
@@ -84,7 +84,7 @@ class Person {
 };
 
 // Inheritance, creating new Employee class which takes from Person class
-
+// Derived Employee Class
 class Employee : public Person {
     public:
     double salary;
@@ -124,6 +124,60 @@ class Employee : public Person {
     
 };
 
+// Derived Patient class from base class Person
+class Patient : public Person {
+    private:
+        string medical_condition;
+        string medication;
+    public:
+    
+    // Constructor
+    Patient(string name, int age, string address, int zipcode, string occupation, string medical_condition, string medication)
+    : Person(name, age, address, zipcode, occupation),
+    medical_condition(medical_condition), medication(medication){
+        //insert code to run when the constructor is called
+    }
+
+    // Getters
+    string get_medical_conditon(){
+        return medical_condition;
+    }
+    
+    string get_medication(){
+        return medication;
+    }
+    
+    // Setters
+    void set_medical_condition(string new_medical_condition){
+        medical_condition = new_medical_condition;
+    }
+    
+    void set_medication(string new_medication){
+        medication = new_medication;
+    }
+    
+};
+
+// Virtual functions are those which are expected to override (replace) a method in a derived class.
+
+// Overriding: When a child class inherits a method from a parent class, the child class wishes to change the behavior or that method.
+
+// Example of Polymorphism
+
+class Animal {
+public:
+    virtual void makeSound(){ // makeSound is a virtual method because we expect that when a child class inherits the method they will change the behavior
+        cout << "Sound sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void makeSound() override { // Dog class overrides the makeSound() method to say "Woof" when called upon in terms of the Dog class.
+        cout << "Woof" << endl;
+    }
+};
+
 // Main function
 int main(){
     
@@ -145,7 +199,7 @@ int main(){
     cout << "My new name is " << p1.get_name() << endl;
     cout << "My new age is " << p1.get_age() << endl;
     
-    // Instatiating an object using the employee class in the order specified in the subclass above
+    // Instatiating an object using the derived employee class of the person class in the order specified in the subclass above
     Employee e1("Joshua", 32, "594 New Street", 12945, "Cybersecurity Engineer", 59493.34, 192938832, "Cybersecurity Engineer II");
     
     // Examples of using getters for the employee class
@@ -159,28 +213,14 @@ int main(){
     e1.set_job_title("Senior Cybersecurity Engineer");
     cout << "My new job title is " << e1.get_job_title() << endl;
     
+    // Instantiating an object from the derived patient class of the person class in order specified in the subclass.
+    Patient o1("Nanami", 34, "102 Pine Street", 12933, "Janitor", "Pulled Muscle", "Aspirin");
+    
+    
+    
     return 0;
 }
 
-// Virtual functions are those which are expected to override (replace) a method in a derived class.
-
-// Overriding: When a child class inherits a method from a parent class, the child class wishes to change the behavior or that method.
-
-// Example of Polymorphism (this should be before the main() function but this is purely for example)
- 
-class Animal {
-public:
-    virtual void makeSound(){ // makeSound is a virtual method because we expect that when a child class inherits the method they will change the behavior
-        cout << "Sound sound" << endl;
-    }
-};
-
-class Dog : public Animal {
-public:
-    void makeSound() override {
-        cout << "Woof" << endl;
-    }
-};
  
  
  
